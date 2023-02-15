42b96f0253842858b27582bf45aaa02a5c37f38c
Ozara Tsubasa <alcantaracloyd26@gmail.com>
2023-02-16 00:08:41 +0800

    update

--- src/direflow-components/widget-component/App.js
+++ src/direflow-components/widget-component/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-import PropTypes from 'prop-types';
 import { Styled } from 'direflow-component';
 import styles from './App.css';
 
@@ -7,27 +6,13 @@ const App = () => {
   useEffect(() => {
     const event = new Event('my-event');
     console.log("EVENT", event)
-  })
+  }, [])
 
   return (
     <Styled styles={styles}>
       <h1>ASDADSADSADSADSAD</h1>
     </Styled>
   );
-};
-
-App.defaultProps = {
-  componentTitle: 'Widget Component',
-  sampleList: [
-    'Create with React',
-    'Build as Web Component',
-    'Use it anywhere!',
-  ],
-}
-
-App.propTypes = {
-  componentTitle: PropTypes.string,
-  sampleList: PropTypes.array,
 };
 
 export default App;
